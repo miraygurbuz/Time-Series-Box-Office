fcccf3d1635d0d811780338073f99e059c133c57
Miray Gürbüz <161487392+miraygurbuz@users.noreply.github.com>
2024-11-13 01:52:00 +0300

    Update settings.py

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,4 +1,4 @@
-DRIVER_PATH = "driver/chromedriver.exe"
+DRIVER_PATH = "../driver/chromedriver.exe"
 BASE_URL = "https://www.boxofficemojo.com/date/?ref_=bo_nb_in_tab"
 YEARS = list(range(2024, 2008, -1))
-OUTPUT_PATH = "data/raw/box_office.csv"
+OUTPUT_PATH = "../data/raw/box_office.csv"
